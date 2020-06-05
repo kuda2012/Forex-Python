788766df6e41a4c23ec883d1f2b7ce18f68c149c
Cody Lee <cody.s.lee@gmail.com>
2020-06-05 06:39:05 -0400

    Adding the tests I had written up with you. Looks like you pulled the other changes from my Google share.

--- test.py
+++ test.py
@@ -2,17 +2,40 @@ from unittest import TestCase
 from flask import session, request
 from app import app
 from currency import Currency
-
+from unittest.mock import MagicMock
 
 app.config['TESTING'] = True
+
 
 class FlaskTests(TestCase):
 
     def test_USD(self):
         """Test to see if one USD translate to one USD"""
         with app.test_client() as client:
-         
-            params = {"start": "USD", "end": "USD", "amount": "2"}
+            app.currency.c.convert = MagicMock(return_value=3)
+
+            params = {"start": "USD", "end": "GBP", "amount": "1"}
             res = client.get("/convert", query_string=params)
             html = res.get_data(as_text=True)
-            self.assertIn("2.00", html)
+            self.assertIn("3.00", html)
+
+    def test_Validate(self):
+        cur = Currency()
+        s, e, a = cur.validate_code("USD", "GBP", "1.0")
+        self.assertFalse(s)
+        self.assertFalse(e)
+        self.assertFalse(a)
+
+    def test_ValidateBadStart(self):
+        cur = Currency()
+        s, e, a = cur.validate_code("XXX", "GBP", "1.0")
+        self.assertTrue(s)
+        self.assertFalse(e)
+        self.assertFalse(a)
+
+    def test_ValidateBadStartEnd(self):
+        cur = Currency()
+        s, e, a = cur.validate_code("XXX", "YYY", "1.0")
+        self.assertTrue(s)
+        self.assertTrue(e)
+        self.assertFalse(a)
